3dbd5139a3b887f1f9d53e427acc3cf98b6265b7
Unknown <2579830610@qq.com>
2018-10-19 01:22:14 +0800

    添加百度统计

---
+++ js/baidutj.js
@@ -0,0 +1,9 @@
+< script >
+  var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1302df24461e27ad683f6ea94b314277";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})(); <
+/script>
