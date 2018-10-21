49ffc5d5026522f286ca377eb4402c755bdd62fb
Unknown <2579830610@qq.com>
2018-10-22 01:47:24 +0800

    测试控制台图片

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-console.log("%c\n", "font-size:99px;background:url('http://www.wuliyingxue.cn/img/logo.png') no-repeat");
+console.log("%c\n", "font-size:112px;background:url('img/logo.png') no-repeat");
 console.log("%c  ___                                 \n"+
    " / _ \\  ___ ___  __ _ _ __   ___ _ __ \n"+
   "| | | |/ __/ _ \\/ _` | '_ \\ / _ \\ '__|\n"+
   "| |_| | (_|  __/ (_| | | | |  __/ |   \n"+
-   " \\___/ \\___\\___|\\__,_|_| |_|\\___|_|  \n","color:blue");
+   " \\___/ \\___\\___|\\__,_|_| |_|\\___|_|  \n","font-size:20px;color:blue");
 console.log("恭喜您喜提彩蛋!!！ \n请多多指教：%cjihuaixi@oceaner.top","color:green");
+console.log("%c\n       ", "font-size:41px;background:url('http://www.wuliyingxue.cn/img/zhidao.png') no-repeat -135px -1px");
