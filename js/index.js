f5c61d9e20a14304119df67c3e15c443b949d759
Unknown <2579830610@qq.com>
2018-10-22 01:35:43 +0800

    添加控制台信息

---
+++ js/index.js
@@ -0,0 +1,7 @@
+console.log("%c\n", "font-size:99px;background:url('http://www.wuliyingxue.cn/img/logo.png') no-repeat");
+console.log("%c  ___                                 \n"+
+   " / _ \\  ___ ___  __ _ _ __   ___ _ __ \n"+
+  "| | | |/ __/ _ \\/ _` | '_ \\ / _ \\ '__|\n"+
+  "| |_| | (_|  __/ (_| | | | |  __/ |   \n"+
+   " \\___/ \\___\\___|\\__,_|_| |_|\\___|_|  \n","color:blue");
+console.log("恭喜您喜提彩蛋!!！ \n请多多指教：%cjihuaixi@oceaner.top","color:green");
