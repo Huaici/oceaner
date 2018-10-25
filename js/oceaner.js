9119ee89f106b1ab386d0c5d1ac3c1fa2a582dc5
Unknown <2579830610@qq.com>
2018-10-26 01:01:29 +0800

    首页添加百度超链接，增加访问量

--- js/oceaner.js
+++ js/oceaner.js
@@ -1,7 +1,10 @@
-console.log("%c  \n", "font-size:112px;background:url('http://www.wuliyingxue.cn/img/logo.png') 25px 0 no-repeat");
-console.log("%c  ___                                              \n"+
- " / _ \\    ___    ___    __ _   _ __     ___   _ __ \n"+
-"| | | |  / __|  / _ \\  / _` | | '_ \\   / _ \\ | '__|\n"+
-"| |_| | | (__  |  __/ | (_| | | | | | |  __/ | |   \n"+
- " \\___/   \\___|  \\___|  \\__,_| |_| |_|  \\___| |_|\n","font-size:20px;color:green;");
-console.log("请多多指教：\n%c  QQ:2579830610\n  邮箱:jihuaixi@oceaner.top","color:green");
+function printInfo() {
+  console.log("%c  \n", "font-size:112px;background:url('http://www.wuliyingxue.cn/img/logo.png') 25px 0 no-repeat");
+  console.log("%c  ___                                              \n"+
+   " / _ \\    ___    ___    __ _   _ __     ___   _ __ \n"+
+  "| | | |  / __|  / _ \\  / _` | | '_ \\   / _ \\ | '__|\n"+
+  "| |_| | | (__  |  __/ | (_| | | | | | |  __/ | |   \n"+
+   " \\___/   \\___|  \\___|  \\__,_| |_| |_|  \\___| |_|\n","font-size:20px;color:green;");
+  console.log("请多多指教：\n%c  QQ:2579830610\n  邮箱:jihuaixi@oceaner.top","color:green");
+}
+printInfo();
